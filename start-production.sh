4237fbed9445b55f3d3daa300c777c4277214ead
Robert Kaye <rob@musicbrainz.org>
2020-11-22 12:21:04 +0100

    Put the datasets on HTTPS

---
+++ start-production.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DOMAIN="datasets.listenbrainz.org"
+
+echo "---- start bono-data-sets"
+docker run -d -p 8000:80 \
+    --name bono-data-sets \
+    --network musicbrainzdocker_default \
+    --env "LETSENCRYPT_HOST=$DOMAIN" \
+    --env "LETSENCRYPT_EMAIL=rob@metabrainz.org" \
+    --env "VIRTUAL_HOST=$DOMAIN" \
+    metabrainz/bono-data-sets
+
+echo "---- start nginx proxy, le"
+docker run -d -p 80:80 -p 443:443 \
+   --name nginx \
+   -v /etc/ssl/le-certs:/etc/nginx/certs:ro \
+   -v /etc/nginx/vhost.d \
+   -v /usr/share/nginx/html \
+   -v /var/run/docker.sock:/tmp/docker.sock:ro \
+   -v `pwd`/nginx/proxy:/etc/nginx/vhost.d/$DOMAIN:ro \
+   --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy \
+   --restart unless-stopped \
+   --network=musicbrainzdocker_default \
+   jwilder/nginx-proxy
+
+docker run -d \
+   --name le \
+   -v /var/run/docker.sock:/var/run/docker.sock:ro \
+   -v /etc/ssl/le-certs:/etc/nginx/certs:rw \
+   --volumes-from nginx \
+   --restart unless-stopped \
+   --network=musicbrainzdocker_default \
+   jrcs/letsencrypt-nginx-proxy-companion
