93fafcc566426222182488e9580d34edda1a2f8a
Robert Kaye <rob@musicbrainz.org>
2021-12-03 15:39:13 +0100

    Use a better genre lookup

--- genre_lookup.py
+++ genre_lookup.py
@@ -29,54 +29,20 @@ class GenreLookupQuery(Query):
         mbids = tuple([ psycopg2.extensions.adapt(p['[recording_mbid]']) for p in params ])
         with psycopg2.connect(config.DB_CONNECT_MB) as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-                query = '''SELECT r.gid::TEXT AS recording_mbid,
-                                  t.name AS tag,
-                                  g.name AS genre
-                             FROM recording_tag rt
-                             JOIN recording r
-                               ON r.id = rt.recording
-                             JOIN tag t
-                               ON rt.tag = t.id
-                        LEFT JOIN genre g
-                               ON g.name = t.name
-                            WHERE rt.count > 0 AND r.gid
-                               IN %s
-                         ORDER BY r.gid, rt.count DESC'''
+                query = """SELECT *
+                             FROM mapping.recording_tags
+                            WHERE recording_mbid in %s"""
 
 
                 args = [mbids]
-                if count > 0:
-                    query += " LIMIT %s"
-                    args.append(count)
-                if offset >= 0:
-                    query += " OFFSET %s"
-                    args.append(offset)
 
+                output = []
                 curs.execute(query, tuple(args))
-                index = {}
                 while True:
                     row = curs.fetchone()
                     if not row:
                         break
 
-                    data = dict(row)
-                    mbid = row['recording_mbid']
-                    if mbid not in index:
-                        index[mbid] = { 'tags': [], 'genres': [] }
+                    output.append(dict(row))
 
-                    index[mbid]['tags'].append(row['tag'])
-                    if row['genre']:
-                        index[mbid]['genres'].append(row['genre'])
-
-                output = []
-                for p in params:
-                    mbid = p['[recording_mbid]']
-                    try:
-                        output.append({ 'recording_mbid': mbid,
-                                        'tags': ",".join(index[mbid]['tags'][:self.MAX_ITEMS_PER_RECORDING]),
-                                        'genres': ",".join(index[mbid]['genres'][:self.MAX_ITEMS_PER_RECORDING])
-                                     })
-                    except KeyError:
-                        pass
-
-        return output
+                return output
