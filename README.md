0a4ff5b24cd16a8a5246dfe4934a1ac5a06c4ef8
Robert Kaye <rob@musicbrainz.org>
2022-04-20 09:17:33 +0000

    Clean up, document and make everything more sane

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-build
------
+# Hosting bono-data-sets
 
-docker build -t metabrainz/bono-data-sets .
+In order to host the data set hoster on wolf, follow the instructions below. For more information
+on the dev server, see SysWiki DevelopmentServer.md .
 
-debug
------
 
-docker run -rm -p 8000:80 --name bono-data-sets --network musicbrainzdocker_default metabrainz/bono-data-sets
+## build
 
-host
-----
+docker-compose build
 
-docker run -d -p 8000:80 --name bono-data-sets --network musicbrainzdocker_default metabrainz/bono-data-sets
+## debug
+
+See the comments in config.py about how to setup DB connect strings to run a test server, then:
+
+```
+python3 -v venv .ve 
+source .ve/bin/activate
+pip3 install -r requirements.tx
+./main.py
+```
+
+## host
+
+To host a container to run indefinitely on the Development Server, do this:
+
+```
+docker-compose run -d
+```
--- main.py
+++ main.py
@@ -1,42 +1,17 @@
 #!/usr/bin/env python3
 
 from datasethoster.main import create_app, register_query
-from msid_mapping import MSIDMappingQuery
-from typesense_test import MBIDMappingSearchTest
-from msid_lookup import MSIDLookupQuery
-from artist_msid_lookup import ArtistMSIDLookupQuery
-from lb_ar_similarity import ArtistSimilarityQuery
-from rec_similarity import RecordingSimilarityQuery
 from ac_name_lookup import ArtistCreditNameLookupQuery
 from area_random_recording import AreaRandomRecordingQuery
 from area_lookup import AreaLookupQuery
-from genre_lookup import GenreLookupQuery
-from top_discoveries import TopDiscoveriesQuery
+#from genre_lookup import GenreLookupQuery
 from artist_country_from_artist_credit_id import ArtistCountryFromArtistCreditIdQuery
-from releases_from_listen_stream import ReleasesFromListenStream
-from top_new_tracks import TopNewTracksQuery
-from top_missed_tracks import TopMissedTracksQuery
-from bpm_key_from_recording import BPMKeyLookupQuery
-from artist_credit_recording_lookup_tgrm import ArtistCreditRecordingLookupTrigramQuery
 
-
-register_query(MSIDMappingQuery())
-register_query(MSIDLookupQuery())
-register_query(ArtistMSIDLookupQuery())
-register_query(ArtistSimilarityQuery())
-register_query(RecordingSimilarityQuery())
 register_query(ArtistCreditNameLookupQuery())
 register_query(AreaRandomRecordingQuery())
 register_query(AreaLookupQuery())
-register_query(GenreLookupQuery())
-register_query(TopDiscoveriesQuery())
+#register_query(GenreLookupQuery())
 register_query(ArtistCountryFromArtistCreditIdQuery())
-register_query(ReleasesFromListenStream())
-register_query(TopNewTracksQuery())
-register_query(TopMissedTracksQuery())
-register_query(BPMKeyLookupQuery())
-register_query(ArtistCreditRecordingLookupTrigramQuery())
-register_query(MBIDMappingSearchTest())
 
 # Needs to be after register_query
 app = create_app('config')
