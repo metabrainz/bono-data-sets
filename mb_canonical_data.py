c968f47ff91ba77558fa272b6679cebcda2d38c2
Alastair Porter <alastair@metabrainz.org>
2022-07-27 11:46:49 +0200

    Rename acr lookup to mb canonical data

--- mb_canonical_data.py
+++ mb_canonical_data.py
@@ -10,10 +10,10 @@ from unidecode import unidecode
 import config
 
 
-class ArtistCreditRecordingLookupQuery(Query):
+class MusicBrainzCanonicalDataLookup(Query):
 
     def names(self):
-        return ("acr-lookup", "MusicBrainz Artist Credit Recording lookup")
+        return ("mbc-lookup", "MusicBrainz Canonical Data lookup")
 
     def inputs(self):
         return ['[artist_credit_name]', '[recording_name]']
@@ -24,8 +24,8 @@ class ArtistCreditRecordingLookupQuery(Query):
 
     def outputs(self):
         return ['index', 'artist_credit_arg', 'recording_arg',
-                'artist_credit_name', 'release_name', 'recording_name', 
-                'artist_credit_id', 'release_mbid', 'recording_mbid']
+                'artist_credit_name', 'release_name', 'recording_name',
+                'artist_mbids', 'release_mbid', 'recording_mbid']
 
     def fetch(self, params, offset=-1, count=-1):
         lookup_strings = []
@@ -40,13 +40,13 @@ class ArtistCreditRecordingLookupQuery(Query):
         with psycopg2.connect(config.DB_CONNECT_MB) as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
                 curs.execute("""SELECT artist_credit_name,
-                                       artist_credit_id,
+                                       artist_mbids,
                                        release_name,
                                        release_mbid,
                                        recording_name,
                                        recording_mbid,
                                        combined_lookup
-                                  FROM mapping.mbid_mapping
+                                  FROM mapping.canonical_musicbrainz_data
                                  WHERE combined_lookup IN %s""", (lookup_strings,))
 
                 results = []
