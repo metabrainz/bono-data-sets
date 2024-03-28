a7fa020d0123253cb7f511edca340691f1be3731
Robert Kaye <rob@musicbrainz.org>
2024-03-28 20:50:39 +0000

    Remove old stuff

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ from datasethoster.main import create_app, register_query
 from ac_name_lookup import ArtistCreditNameLookupQuery
 from area_random_recording import AreaRandomRecordingQuery
 from area_lookup import AreaLookupQuery
-#from genre_lookup import GenreLookupQuery
 from artist_country_from_artist_credit_id import ArtistCountryFromArtistCreditIdQuery
 from mb_canonical_data import MusicBrainzCanonicalDataLookup
-#from rec_similarity import RecordingSimilarityQuery
 from upcoming_releases import UpcomingReleasesQuery
 from mb_canonical_data import MusicBrainzCanonicalDataLookup
 from releases_from_recording import ReleasesFromRecordingQuery
-from top_discoveries import TopDiscoveriesQuery
-from popular_recordings import PopularRecordingsByArtistQuery
 from artist_lookup import ArtistLookupQuery
 from popular_releases_by_listencount import PopularReleasesFromTotalListenCountQuery
 from popular_releases_by_listeners import PopularReleasesFromTotalListenersQuery
@@ -23,6 +19,7 @@ from popular_artists_by_listeners import PopularArtistsFromTotalListenersQuery
 from popular_recordings_by_listeners import PopularRecordingsFromTotalListenersQuery
 from popular_recordings_by_listencount import PopularRecordingsFromTotalListenCountQuery
 from feedback_lookup import FeedbackLookupQuery
+from similar import SimilarArtistSelectorQuery
 #from bulk_recording_lookup import BulkRecordingLookupQuery
 
 psycopg2.extras.register_uuid()
@@ -30,14 +27,10 @@ psycopg2.extras.register_uuid()
 register_query(MusicBrainzCanonicalDataLookup())
 register_query(AreaRandomRecordingQuery())
 register_query(AreaLookupQuery())
-#register_query(GenreLookupQuery())
 register_query(ArtistCountryFromArtistCreditIdQuery())
 register_query(MusicBrainzCanonicalDataLookup())
-#register_query(RecordingSimilarityQuery())
 register_query(UpcomingReleasesQuery())
 register_query(ReleasesFromRecordingQuery())
-register_query(TopDiscoveriesQuery())
-register_query(PopularRecordingsByArtistQuery())
 register_query(ArtistLookupQuery())
 register_query(PopularReleasesFromTotalListenCountQuery())
 register_query(PopularReleasesFromTotalListenersQuery())
@@ -46,7 +39,7 @@ register_query(PopularArtistsFromTotalListenersQuery())
 register_query(PopularRecordingsFromTotalListenersQuery())
 register_query(PopularRecordingsFromTotalListenCountQuery())
 register_query(FeedbackLookupQuery())
-#register_query(BulkRecordingLookupQuery())
+register_query(SimilarArtistSelectorQuery())
 
 # Needs to be after register_query
 app = create_app('config')
--- releases_from_recording.py
+++ releases_from_recording.py
@@ -1,8 +1,9 @@
+from uuid import UUID
 import psycopg2
 import psycopg2.extras
+from werkzeug.exceptions import BadRequest
 from datasethoster import Query
 import config
-
 
 class ReleasesFromRecordingQuery(Query):
 
@@ -21,6 +22,12 @@ class ReleasesFromRecordingQuery(Query):
     def fetch(self, params, offset=-1, count=-1):
 
         mbids = [p['[recording_mbid]'] for p in params]
+        try:
+            for mbid in mbids:
+                _ = UUID(mbid)
+        except ValueError:
+            raise BadRequest(f"MBID {str(mbid)} is not a valid recording_mbid")
+
         with psycopg2.connect(config.DB_CONNECT_MB) as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
 
@@ -39,27 +46,41 @@ class ReleasesFromRecordingQuery(Query):
                                           WHERE r.gid IN %s
                                 ), release_recordings AS (
                                          SELECT rgid.release_group_mbid
+                                              , rgpt.name AS type
                                               , rl.gid AS release_mbid
+                                              , rl.name AS release_name
                                               , array_agg(jsonb_build_array(r.gid,            
-                                                                             r.name,           
-                                                                             r.length,   
-                                                                             t.position, 
-                                                                             m.position)
+                                                                            r.name,           
+                                                                            ac.name,
+                                                                            r.length,   
+                                                                            t.position, 
+                                                                            m.position,
+                                                                            mf.name)
                                                           ORDER BY m.position, t.position) AS release         
                                            FROM recording r
+                                           JOIN artist_credit ac
+                                             ON ac.id = r.artist_credit
                                            JOIN track t
                                              ON t.recording = r.id
                                            JOIN medium m
                                              ON t.medium = m.id
+                                           JOIN medium_format mf
+                                             ON m.format = mf.id
                                            JOIN release rl
                                              ON m.release = rl.id
                                            JOIN release_group_mbids rgid
                                              ON rl.release_group = rgid.release_group_id
+                                           JOIN release_group rg
+                                             ON rg.id = rl.release_group
+                                           JOIN release_group_primary_type rgpt
+                                             ON rg.type = rgpt.id
                                        GROUP BY rgid.release_group_mbid  
+                                              , rgpt.name
                                               , rl.gid
+                                              , rl.name
                                  )
                                          SELECT rr.release_group_mbid::TEXT
-                                              , array_agg(jsonb_build_array(rr.release_mbid, rr.release)) AS releases
+                                              , array_agg(jsonb_build_array(rr.release_mbid::TEXT, rr.release_name, rr.release, rr.type)) AS releases
                                            FROM release_recordings rr
                                        GROUP BY rr.release_group_mbid
                                        """, (tuple(mbids),))
@@ -73,8 +94,10 @@ class ReleasesFromRecordingQuery(Query):
                     row = dict(row)
                     releases = {}
                     for r in row["releases"]:
-                        releases[r[0]] = r[1]
+                        releases[r[0]] = { "release_name": r[1], "recordings": r[2] }
 
-                    results.append({ "release_group_mbid": row["release_group_mbid"], "releases": releases })
+                    results.append({ "release_group_mbid": row["release_group_mbid"],
+                                     "type": r[3],
+                                     "releases": releases })
 
                 return results
